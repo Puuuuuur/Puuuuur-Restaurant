017cff3ac6f1e26c0a72224715d3e01cb5c1764f
Puuuuuur <pu1746275769@163.com>
2025-08-16 14:42:33 +0800

    新增员工2

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -88,6 +88,7 @@ public class EmployeeController {
     @ApiOperation("新增员工")
     public Result save(@RequestBody EmployeeDTO employeeDTO) {
         log.info("新增员工，员工数据：{}", employeeDTO);
+        System.out.println("线程id2：{}" + Thread.currentThread().getId());
         employeeService.save(employeeDTO);
         return Result.success();
     }
