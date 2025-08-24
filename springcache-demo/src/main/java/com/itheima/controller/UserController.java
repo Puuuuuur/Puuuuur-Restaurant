37273bf40fe099585f200d222fea8b3eec7ed95c
Puuuuuur <pu1746275769@163.com>
2025-08-24 11:24:46 +0800

    feat: Spring Cache学习

--- springcache-demo/src/main/java/com/itheima/controller/UserController.java
+++ springcache-demo/src/main/java/com/itheima/controller/UserController.java
@@ -4,6 +4,9 @@ import com.itheima.entity.User;
 import com.itheima.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,26 +17,47 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
+    /**
+     * 新增用户
+     * @param user
+     * @return
+     */
     @PostMapping
+    @CachePut(cacheNames = "userCache",key = "#user.id") //如果使用Spring Cache缓存数据，key的生成：userCache::{{user.id}}
     public User save(@RequestBody User user){
         userMapper.insert(user);
         return user;
     }
 
+    /**
+     * 根据id查询用户
+     * @param id
+     * @return
+     */
+    @GetMapping
+    @Cacheable(cacheNames = "userCache",key = "#id") //key的生成：userCache::1 -> userCache::{{id}}
+    public User getById(Long id){
+        User user = userMapper.getById(id);
+        return user;
+    }
+
+    /**
+     * 根据id删除用户
+     * @param id
+     */
+    @CacheEvict(cacheNames = "userCache",key = "#id") //key的生成：userCache::1 -> userCache::{{id}}
     @DeleteMapping
     public void deleteById(Long id){
         userMapper.deleteById(id);
     }
 
+    /**
+     * 删除所有用户
+     */
+    @CacheEvict(cacheNames = "userCache",allEntries = true)
 	@DeleteMapping("/delAll")
     public void deleteAll(){
         userMapper.deleteAll();
-    }
-
-    @GetMapping
-    public User getById(Long id){
-        User user = userMapper.getById(id);
-        return user;
     }
 
 }
